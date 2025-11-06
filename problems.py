b1df9527a04694c1e64b33c33874ab8c25f40a77
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-11-06 23:59:23 +0530

    Update problems.py

--- problems.py
+++ problems.py
@@ -41,4 +41,10 @@ else:
     print("The specified directory does not exist.")
 
 """
+
 """
+# The provided code stub reads an integer, n, from STDIN. For all non-negative integers i<n, print i^2.
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
