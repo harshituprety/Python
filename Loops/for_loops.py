a70c762fc218deb26d0b4968a77d013ab5a93f8a
Hi-star <110157158+harshituprety@users.noreply.github.com>
2025-12-13 13:19:59 +0530

    Add files via upload

---
+++ Loops/for_loops.py
@@ -0,0 +1,21 @@
+name = 'Avinash'
+for i in name:
+  print(i)
+  if(i=='v'):
+    print('This is something Awesome!')
+
+colors = ["red","voilet","yellow","green"]
+
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
+
+for k in range(5):
+  print(k+1)
+
+for k in range(1,2001):
+  print(k)
+
+for k in range (1,15,2):
+  print(k)
