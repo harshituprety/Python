8b4456ba78ea32ab10ad068ef710df166a7ec3b5
Hi-star <110157158+harshituprety@users.noreply.github.com>
2026-01-03 13:00:39 +0530

    Add files via upload

---
+++ Strings/docstrings.py
@@ -0,0 +1,7 @@
+# The string literals that appear right after the definition of a function, method, class or module 
+
+def square(a):
+  '''Takes in a number a, return the square of a'''
+  print(a**2)
+square(6)
+print(square.__doc__)
