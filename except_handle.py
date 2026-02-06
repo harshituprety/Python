ae04b3ab4dabe595edd049820c143544d5fe7a3a
Hi-star <110157158+harshituprety@users.noreply.github.com>
2026-02-06 23:18:52 +0530

    Add files via upload

---
+++ except_handle.py
@@ -0,0 +1,19 @@
+# Exception handling is the process of responding to unwanted or unexpected events when a computer program runs. Exception handling deals with these events to avoid the program or system crashing, and without this process, exceptions would disrupt the normal operation of a program.
+
+try:
+  a = int(input('Enter the number: '))
+  print(f"Multiplication table of {a} is : ")
+
+  for i in range(1,11):
+    print(f"{a} X {i} = {a*i}")
+except:
+  print("Invalid Input")
+  
+print("End of program")
+
+try:
+    num = int(input('Enter an integer:'))
+    a = [6,3]
+    print(a[num])
+except ValueError:
+    print("Number entered is not an integer.")
